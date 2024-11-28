215896f56df9f9b9971a2962f46b21f091e9ed43
Zaajil_Jilu <zaajiljilu@gmail.com>
2024-11-28 17:35:29 +0530

    First commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,11 @@ module.exports = {
       colors: {
         primary: "#e57ca0",  // Main pink
         secondary: "#856494", // Violet
-        accent: "#e3c069",   // Gold
+        accent: "#fffbeb",   // Gold
         lightAccent: "#fae4ab", // Light cream
         darkAccent: "#40105e", // Dark purple
         paleAccent: "#fae4a",  // Pale cream
-      },
+        },
     },
   },
   plugins: [],
